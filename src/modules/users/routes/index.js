18e5ec6a5a12b70606f82d68d8709170f46ffd75
joao <joao_gabriel200@hotmail.com>
2019-07-19 21:26:16 -0300

    routes

--- src/modules/users/routes/index.js
+++ src/modules/users/routes/index.js
@@ -5,7 +5,13 @@ import functions from '../helpers/functions'
 const Routes = Router()
 
 Routes
+  .get('/paginated', functions.getWithPaginate)
+  .get('/', functions.get)
+  .get('/:id', functions.getById)
   .post('/', functions.post)
-  .patch('/', functions.patch)
+  .post('/many', functions.insertMany)
+  .patch('/:id', functions.patch)
+  .patch('/activateDeactivate/:id', functions.activateDeactivate)
+  .delete('/:id', functions.delete)
 
 export default Routes
