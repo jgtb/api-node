1f680be6815810f86f0fe7085c68c0d1ed9cae35
jgtb <joao_gabriel200@hotmail.com>
2019-10-11 19:11:37 -0300

    att

--- seeds/adverts.js
+++ seeds/adverts.js
@@ -7,6 +7,8 @@ import {
   BrandsSchema,
   ModesSchema,
   VersionsSchema,
+  CategoriesSchema,
+  OptionalsSchema,
   FuelsSchema,
   ColorsSchema
 } from '../src/modules/catalogs/models'
@@ -23,27 +25,29 @@ export default async (Faker) => {
     const brandModel = await getRandom({ Schema: BrandsSchema, $match: { vehicle: Types.ObjectId(vehicleModel._id) } })
     const modeModel = await getRandom({ Schema: ModesSchema, $match: { brand: Types.ObjectId(brandModel._id) } })
     const versionModel = await getRandom({ Schema: VersionsSchema, $match: { mode: Types.ObjectId(modeModel._id) } })
+    const categoryModel = await getRandom({ Schema: CategoriesSchema, $match: { vehicle: Types.ObjectId(vehicleModel._id) } })
+    const optionalsModels = await getRandom({ Schema: OptionalsSchema, size: 5, $match: { category: Types.ObjectId(categoryModel._id) } })
     const fuelModel = await getRandom({ Schema: FuelsSchema })
     const colorModel = await getRandom({ Schema: ColorsSchema })
+
+    const optionals = optionalsModels.map(({ _id }) => _id)
+
     await new AdvertsSchema({
       user: userModel._id,
       name: Faker.name.findName(),
       price: Faker.random.number({ min: 100, max: 500 }),
-      year: '2019',
-      modelYear: '2017',
       vehicle: vehicleModel._id,
       brand: brandModel._id,
       mode: modeModel._id,
       version: versionModel._id,
+      category: categoryModel._id,
+      optionals,
       fuel: fuelModel._id,
       color: colorModel._id,
-      km: Faker.random.number({ min: 100, max: 500 }),
-      board: Faker.name.findName(),
-      doors: Faker.random.number({ min: 1, max: 4 }),
-      motor: Faker.name.findName(),
-      valves: Faker.name.findName(),
-      photos: [],
-      soldOn: Faker.random.boolean() ? new Date() : null,
+      // km: Faker.random.number({ min: 100, max: 500 }),
+      // board: Faker.name.findName(),
+      // doors: Faker.random.number({ min: 1, max: 4 }),
+      // photos: [],
       financed: Faker.random.boolean(),
       acceptExchange: Faker.random.boolean(),
       owner: Faker.random.boolean(),
